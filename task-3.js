6c2e96a09e5d2d33b372ad58a196d7bcd49f3b72
Juga Paazmaya <paazmaya@yahoo.com>
2014-09-09 20:22:09 +0300

    Replace JS Date calls with moment

--- task-3.js
+++ task-3.js
@@ -1,16 +1,12 @@
 var m = require('./last-mod');
+var moment = require('moment');
 
 var filename = process.argv[2];
 var timestamp = m(filename);
 if (timestamp !== false) {
 	var date = new Date(timestamp);
 	
-	var fin = date.getDate() + '.' + 
-		(date.getMonth() + 1) + '.' +
-		date.getFullYear();
-		
-	fin += '  ' + date.getHours() + '.' +
-		date.getMinutes();
+	var fin = moment(date).format('D.M.YYYY  H.m');
 	
 	console.log(fin);
 }
